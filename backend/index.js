f3541960c4bb2d44064605902872ffa31c829ee8
Steve-omush <93277164+Steve-omush@users.noreply.github.com>
2025-09-29 23:04:15 +0300

    Update index.js by updating port

--- backend/index.js
+++ backend/index.js
@@ -134,7 +134,7 @@ app.patch("/updateBook/:id", async (req, res) => {
 });
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8800;
 if (!PORT) {
   throw new Error("PORT environment variable not set");
 }
